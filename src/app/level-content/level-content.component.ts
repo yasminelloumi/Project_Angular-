44cab3ba98ae1139777aed42b334e6cb3639a48e
ZainebAffes <zaineb.affes20@gmail.com>
2025-04-25 23:54:05 +0100

    merge non fonctionnel

--- src/app/level-content/level-content.component.ts
+++ src/app/level-content/level-content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CourseService } from '../services/course.service';
+import { CoursService } from '../services/course.service';
 
 @Component({
   selector: 'app-level-content',
@@ -36,7 +36,7 @@ export class LevelContentComponent implements OnInit {
   levelId: number = 0;
   etape: any = null;
 
-  constructor(private route: ActivatedRoute, private courseService: CourseService) {}
+  constructor(private route: ActivatedRoute, private courseService: CoursService) {}
 
   ngOnInit(): void {
     this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
