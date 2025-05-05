dc3962824c5e7b7658c391e25cd9b2359d025f2b
yasmine <yelloumi640@gmail.com>
2025-05-05 13:43:17 +0100

    refactor: remove MapsComponent and related files from admin layout

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -9,7 +9,7 @@ import { IconsComponent } from '../../icons/icons.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { CourseLevelComponent } from 'app/course-level/course-level.component';
-import { MapsComponent } from 'app/maps/maps.component';
+
 import { LevelContentComponent } from 'app/level-content/level-content.component';
 const routes: Routes = [
   {
@@ -81,7 +81,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'table-list',     component: TableListComponent },
     { path: 'typography',     component: TypographyComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'upgrade',        component: UpgradeComponent },
     { path: 'Cours/:id',        component: CourseLevelComponent },
